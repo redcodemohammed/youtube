0519b9ec684fe2457ae7f1ea0a150531ecacf15a
redcodemohammed <redcodemohammed@gmail.com>
2020-04-27 23:43:18 +0300

    first commit

---
+++ Errors/Main.java
@@ -0,0 +1,82 @@
+import java.util.Scanner;
+
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        // 1- syntax errors:
+
+        // Invalid letters case:
+        // System.out.println("😁");
+
+        // -----------------------------------------------
+
+        // Missing semi-colon:
+        // System.out.println("👋 world");
+
+        // -----------------------------------------------
+
+        // Missing closing brace:
+        // if (5 > 3) {
+        // System.out.println("5 is greater than 3! 🥳");
+        // }
+        // -----------------------------------------------
+
+        // Missing closing parenthesis:
+        // System.out.println("This is my cat: 🐈");
+
+        // -----------------------------------------------
+
+        // Missing a quotation mark:
+        // String myName = "Mohammed";
+
+        // -----------------------------------------------
+
+        // Using keywords as identifiers:
+        // int public = 29;
+        // boolean static = false;
+
+        // ----------------------------------------------------------------------------------------------
+
+        // 2- other types of errors:
+
+        // Type mismatch:
+        // boolean h = false;
+
+        // -----------------------------------------------
+
+        // Duplicate local variable:
+        // String firstTime = "1️⃣";
+        // String secondTime = "2️⃣";
+
+        // -----------------------------------------------
+
+        // Cannot be resolved to a variable:
+        // String name = "Ali";
+        // System.out.println(name);
+
+        // -----------------------------------------------
+
+        // type (some type) is not applicable for the arguments:
+        // String emojis = "5566";
+        // char iraq = emojis.charAt(3);
+        // System.out.println(iraq);
+
+        // -----------------------------------------------
+
+        // Cannot be resolved to a type:
+        // Scanner input = new Scanner(System.in);
+
+        // -----------------------------------------------
+
+        // Invalid assignment statement:
+        // 5 = 10;
+        // 'n' = '3';
+        // "dog" = "🐕";
+
+        // -----------------------------------------------
+
+    }
+}
